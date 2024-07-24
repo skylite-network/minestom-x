bd592354089b681d568d3e68011c8aed223c116d
mudkipdev <mudkip@mudkip.dev>
2024-07-24 17:22:32 -0600

    rebrand

--- jmh-benchmarks/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("me.champeau.jmh") version ("0.7.2")
-}
-
-dependencies {
-    jmhImplementation(rootProject)
-    jmh(libs.jmh.core)
-    jmhAnnotationProcessor(libs.jmh.annotationprocessor)
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,4 @@ rootProject.name = "minestom"
 
 include("testing")
 include("code-generators")
-include("jmh-benchmarks")
-include("jcstress-tests")
-
 include("demo")
